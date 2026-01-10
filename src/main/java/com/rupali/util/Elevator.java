2c063b31fa2d6f9726b523873c43ef3e7d6c0289
rupali bizoara <rupalibizoara472@gmail.com>
2026-01-11 00:55:54 +0530

    my 8rd message

--- src/main/java/com/rupali/util/Elevator.java
+++ src/main/java/com/rupali/util/Elevator.java
@@ -1,42 +1,50 @@
 package com.rupali.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Elevator {
-	
-	
-	@A
+
+	@Id
 	private int A;
 	private String B;
 	private String C;
+	
 	private String D;
+
 	public int getA() {
 		return A;
 	}
+
 	public void setA(int a) {
 		A = a;
 	}
+
 	public String getB() {
 		return B;
 	}
+
 	public void setB(String b) {
 		B = b;
 	}
+
 	public String getC() {
 		return C;
 	}
+
 	public void setC(String c) {
 		C = c;
 	}
+
 	public String getD() {
 		return D;
 	}
+
 	public void setD(String d) {
 		D = d;
-	}  
-	
-	
-
+	}
 
 }
